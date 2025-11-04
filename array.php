a0dbce83fabf70ddcd4a9577ff83f25dbfb7d978
Dwi indriani <syahputrid290@gmaol.com>
2025-11-04 23:27:12 +0700

    Logika pembelian

--- array.php
+++ array.php
@@ -8,4 +8,23 @@ echo "<br>"; // ganti hr jadi jarak baris kosong
 
 // Daftar produk dan harga
 $nama_barang  = ["👕 Baju", "🧕 Hijab", "🧥 Jaket", "👟 Sepatu", "👜 Tas"];
-$harga_barang = [54000, 15000, 84000, 125000, 1500000];
+$harga_barang = [54000, 15000, 84000, 125000, 150000];
+
+// Acak urutan produk agar hasil berbeda setiap dijalankan
+shuffle($nama_barang);
+
+// Buat variasi acak jumlah produk yang dibeli (1–5 produk)
+$jumlah_produk = rand(1, count($nama_barang));
+
+// Array penampung hasil
+$jumlah_beli = [];
+$total = [];
+$grandtotal = 0;
+
+// Loop untuk membuat data penjualan acak TANPA produk duplikat
+for ($i = 0; $i < $jumlah_produk; $i++) {
+    $jumlah_beli[$i] = rand(1, 5); // jumlah beli acak
+    $index_harga = array_search($nama_barang[$i], ["👕 Baju", "🧕 Hijab", "🧥 Jaket", "👟 Sepatu", "👜 Tas"]);
+    $total[$i] = $harga_barang[$index_harga] * $jumlah_beli[$i];
+    $grandtotal += $total[$i];
+}
