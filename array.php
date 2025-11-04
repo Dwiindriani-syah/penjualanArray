727a6d03dea1fbaca8cf0a5f32f914e52f64e27c
Dwi indriani <syahputrid290@gmaol.com>
2025-11-04 22:53:31 +0700

    Menambahkan Setup Awal

---
+++ array.php
@@ -0,0 +1,11 @@
+<?php
+// Judul program
+echo "<div style='font-family:Courier New, monospace; text-align:center;'>";
+echo "<h3>🧾✨ JAYA MART ✨🧾</h3>";
+echo "🏠 Jl. Merdeka No. 45, Jakarta<br>";
+echo "☎️  (021) 1234567<br>";
+echo "<br>"; // ganti hr jadi jarak baris kosong
+
+// Daftar produk dan harga
+$nama_barang  = ["👕 Baju", "🧕 Hijab", "🧥 Jaket", "👟 Sepatu", "👜 Tas"];
+$harga_barang = [54000, 15000, 84000, 125000, 1500000];
